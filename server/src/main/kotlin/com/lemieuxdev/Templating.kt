ffc36c64c317ff18bcf42f1826c0d3ca5375e320
Eric Lemieux <eric@lemieuxdev.com>
2025-01-15 20:43:36 -0500

    start of game

--- server/src/main/kotlin/com/lemieuxdev/Templating.kt
+++ server/src/main/kotlin/com/lemieuxdev/Templating.kt
@@ -18,14 +18,28 @@ import kotlin.time.Duration.Companion.seconds
 import kotlinx.html.*
 import org.slf4j.event.*
 
+class Game(var foo: Int = 100, var player: Player?, var monster: Monster?)
+class Player(var attack:Int=10)
+fun Player.hitMonster(monster: Monster) {
+    monster.health-=attack
+}
+class Monster(var health: Int = 50)
+val game = Game(player = Player(), monster = Monster())
+
 fun Application.configureTemplating() {
     routing {
         val random = Random(System.currentTimeMillis())
 
         get("/") {
             call.respondHtml {
-                leaderboardPage(random)
+                body{
+                    h1{
+                        +"The monster has ${game.monster?.health} HP"
+                    }
+                }
             }
+
+            game.player?.hitMonster(game.monster!!)
         }
 
         get("/more-rows") {
@@ -33,7 +47,7 @@ fun Application.configureTemplating() {
                 body {
                     table {
                         tbody {
-                            randomRows(random)
+                            +game.foo
                         }
                     }
                 }
